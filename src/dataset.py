3598e4db04885a23fcc961f0cc1bf4fff55d5324
Daniel Abib <danielsabib@gmail.com>
2021-10-20 12:22:19 -0300

    Ongoing impl. of dataset and loader

---
+++ src/dataset.py
@@ -0,0 +1,41 @@
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torch.utils.data.dataset import T_co
+
+# from src.load_data import load_train_data, load_test_data, load_train_labels, load_test_labels
+
+from load_data import load_train_data, load_test_data, load_train_labels, load_test_labels
+
+
+class MNIST(Dataset):
+
+    def __init__(self, 
+            data: np.ndarray, 
+            labels: np.ndarray) -> None:
+        if len(data) != len(labels):
+            raise ValueError('data and labels must be the same length. '
+                             f'{len(data)} != {len(labels)}')
+        self.data = data
+        self.labels = labels
+
+    def __getitem__(self, index: int) -> T_co:
+        image = self.data[index]
+        label = self.labels[index]
+        return (image, label)
+
+    def __len__(self):
+        return len(self.data)
+
+
+def get_train_dataloader(batch_size: int) -> DataLoader:
+    return DataLoader(
+        MNIST(load_train_data(), load_train_labels()),
+        batch_size=batch_size,
+        shuffle=True)
+
+def get_test_dataloader(batch_size: int) -> DataLoader:
+    return DataLoader(
+        MNIST(load_test_data(), load_test_labels()),
+        batch_size=batch_size,
+        shuffle=False)
--- src/load_data.py
+++ src/load_data.py
@@ -2,49 +2,56 @@ import gzip
 import numpy as np
 
 
-def load_train_data():
+def load_train_data() -> np.ndarray:
     row_size = col_size = 28
     num_samples = 60_000
 
-    f = gzip.open('../data/train-images-idx3-ubyte.gz', 'r')
+    f = gzip.open('data/train-images-idx3-ubyte.gz', 'r')
     f.read(16)
     buf = f.read(row_size * col_size * num_samples)
     data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
     return data.reshape((num_samples, row_size, col_size))
 
 
-def load_test_data():
+def load_test_data() -> np.ndarray:
     row_size = col_size = 28
     num_samples = 10_000
 
-    f = gzip.open('../data/t10k-images-idx3-ubyte.gz', 'r')
+    f = gzip.open('data/t10k-images-idx3-ubyte.gz', 'r')
     f.read(16)
     buf = f.read(row_size * col_size * num_samples)
     data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
     return data.reshape((num_samples, row_size, col_size))
 
 
-def load_train_labels():
-    f = gzip.open('../data/train-labels-idx1-ubyte.gz', 'r')
+def load_train_labels() -> np.ndarray:
+    f = gzip.open('data/train-labels-idx1-ubyte.gz', 'r')
     f.read(8)
     buf = f.read()
     return np.frombuffer(buf, dtype=np.dtype(np.uint8).newbyteorder(">"))
 
 
-def load_test_labels():
-    f = gzip.open('../data/t10k-labels-idx1-ubyte.gz', 'r')
+def load_test_labels() -> np.ndarray:
+    f = gzip.open('data/t10k-labels-idx1-ubyte.gz', 'r')
     f.read(8)
     buf = f.read()
     return np.frombuffer(buf, dtype=np.dtype(np.uint8).newbyteorder(">"))
 
 
-def load_data():
+def load_data() -> np.ndarray:
     return load_train_data(), load_test_data(), load_train_labels(), load_test_labels()
 
 
 if __name__ == '__main__':
-    train_data, test_data, train_label, test_label = load_data()
+    train_data, test_data, train_labels, test_labels = load_data()
     print(train_data.shape)
     print(test_data.shape)
-    print(train_label.shape)
-    print(test_label.shape)
+    print(train_labels.shape)
+    print(test_labels.shape)
+    print(train_labels[:5])
+    print(test_labels[:5])
+
+    import matplotlib.pyplot as plt
+    image = np.asarray(test_data[2]).squeeze()
+    plt.imshow(image)
+    plt.show()
