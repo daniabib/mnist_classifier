67c5d958b1ab857b702ae0381c4701fae43dd204
Daniel Abib <danielsabib@gmail.com>
2021-10-26 17:17:26 -0300

    Included mypy configuration

--- src/load_data.py
+++ src/load_data.py
@@ -1,8 +1,10 @@
 import gzip
+from typing import Any
 import numpy as np
+import numpy.typing as npt
 
 
-def load_train_data() -> np.ndarray:
+def load_train_data() -> npt.NDArray[Any]:
     row_size = col_size = 28
     num_samples = 60_000
 
@@ -13,7 +15,7 @@ def load_train_data() -> np.ndarray:
     return data.reshape((num_samples, row_size, col_size))
 
 
-def load_test_data() -> np.ndarray:
+def load_test_data() -> npt.NDArray[Any]:
     row_size = col_size = 28
     num_samples = 10_000
 
@@ -24,34 +26,34 @@ def load_test_data() -> np.ndarray:
     return data.reshape((num_samples, row_size, col_size))
 
 
-def load_train_labels() -> np.ndarray:
+def load_train_labels() -> npt.NDArray[Any]:
     f = gzip.open('data/train-labels-idx1-ubyte.gz', 'r')
     f.read(8)
     buf = f.read()
     return np.frombuffer(buf, dtype=np.dtype(np.uint8).newbyteorder(">"))
 
 
-def load_test_labels() -> np.ndarray:
+def load_test_labels() -> npt.NDArray[Any]:
     f = gzip.open('data/t10k-labels-idx1-ubyte.gz', 'r')
     f.read(8)
     buf = f.read()
     return np.frombuffer(buf, dtype=np.dtype(np.uint8).newbyteorder(">"))
 
 
-def load_data() -> np.ndarray:
+def load_data():
     return load_train_data(), load_test_data(), load_train_labels(), load_test_labels()
 
 
-if __name__ == '__main__':
-    train_data, test_data, train_labels, test_labels = load_data()
-    print(train_data.shape)
-    print(test_data.shape)
-    print(train_labels.shape)
-    print(test_labels.shape)
-    print(train_labels[:5])
-    print(test_labels[:5])
+# if __name__ == '__main__':
+#     train_data, test_data, train_labels, test_labels = load_data()
+#     print(train_data.shape)
+#     print(test_data.shape)
+#     print(train_labels.shape)
+#     print(test_labels.shape)
+#     print(train_labels[:5])
+#     print(test_labels[:5])
 
-    import matplotlib.pyplot as plt
-    image = np.asarray(test_data[2]).squeeze()
-    plt.imshow(image)
-    plt.show()
+#     import matplotlib.pyplot as plt
+#     image = np.asarray(test_data[2]).squeeze()
+#     plt.imshow(image)
+#     plt.show()
